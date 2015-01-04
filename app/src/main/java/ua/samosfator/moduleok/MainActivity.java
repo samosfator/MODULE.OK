e3759a70e8e75b9cce8057368e1f1ffba663baf9
samosfator <golubev.vld@gmail.com>
2015-01-04 20:54:59 +0200

    Implement saving drawer state after rotate

--- app/src/main/java/ua/samosfator/moduleok/MainActivity.java
+++ app/src/main/java/ua/samosfator/moduleok/MainActivity.java
@@ -20,10 +20,11 @@ public class MainActivity extends ActionBarActivity {
         toolbar = (Toolbar) findViewById(R.id.app_toolbar);
         toolbar.setTitle(R.string.main_toolbar_text);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer_fragment);
-        drawerFragment.setup((DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
+        drawerFragment.setup(R.id.navigation_drawer_fragment, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
     }
 
 
--- app/src/main/java/ua/samosfator/moduleok/NavigationDrawerFragment.java
+++ app/src/main/java/ua/samosfator/moduleok/NavigationDrawerFragment.java
@@ -1,9 +1,12 @@
 package ua.samosfator.moduleok;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,13 +14,78 @@ import android.view.ViewGroup;
 
 public class NavigationDrawerFragment extends Fragment {
 
+    public static final String PREF_FILE_NAME = "NavigationDrawerFragment";
+    public static final String KEY_USER_SAW_DRAWER = "user_saw_drawer";
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+
+    private boolean mUserSawDrawer;
+    private boolean mFromSavedInstanceState;
+
+    private View containerView;
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mUserSawDrawer = Boolean.valueOf(readFromPreferences(getActivity(), KEY_USER_SAW_DRAWER, "false"));
+        if (savedInstanceState == null) {
+            mFromSavedInstanceState = true;
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
     }
 
-    public void setup(DrawerLayout drawerLayout, Toolbar toolbar) {
+    public void setup(int fragmentId, DrawerLayout drawerLayout, Toolbar toolbar) {
+        containerView = getActivity().findViewById(fragmentId);
+        mDrawerLayout = drawerLayout;
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(),
+                drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                if (!mUserSawDrawer) {
+                    mUserSawDrawer = true;
+                    saveToPreferences(getActivity(), KEY_USER_SAW_DRAWER, String.valueOf(mUserSawDrawer));
+                }
+                getActivity().invalidateOptionsMenu();
+            }
 
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getActivity().invalidateOptionsMenu();
+            }
+        };
+
+        if (!mUserSawDrawer && !mFromSavedInstanceState) {
+            mDrawerLayout.openDrawer(containerView);
+        }
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mDrawerToggle.syncState();
+            }
+        });
+    }
+
+    public static void saveToPreferences(Context context, String preferenceName, String preferenceValue) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(preferenceName, preferenceValue);
+        editor.apply();
+    }
+
+    public static String readFromPreferences(Context context, String preferenceName, String defaultValue) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(preferenceName, defaultValue);
     }
 }
