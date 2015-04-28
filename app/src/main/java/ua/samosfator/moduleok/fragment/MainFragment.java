4669d5e6226a80f57b917e42f86a72967cf574a9
vlad <golubev.vld@gmail.com>
2015-04-28 14:44:49 +0300

    Create a skeleton for future fragment

---
+++ app/src/main/java/ua/samosfator/moduleok/fragment/MainFragment.java
@@ -0,0 +1,21 @@
+package ua.samosfator.moduleok.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ua.samosfator.moduleok.R;
+
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
